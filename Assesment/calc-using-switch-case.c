b7197ea2540c93192891eebb616522258b4bbc8f
nikhildodiya <128133111+nikhildodiya@users.noreply.github.com>
2023-06-02 21:26:11 +0530

    calc-using-switch-case.c

--- Assesment/calc-using-switch-case.c
+++ Assesment/calc-using-switch-case.c
@@ -1,38 +1,64 @@
 // WAP To Make Simple Calculater Using Switch Case
 
 #include<stdio.h>
-int main()
+           
+        int calc; // Variable To Store The Value
+        int no1, no2;
+int add() // Using Function For addition
+
+            printf("\naddition is : %d", no1 + no2);
+}
+
+int sub() // Using Function For Substraction
 {
-    int calc;
-    int no1, no2;
+            printf("\nsubstraction is : %d", no1 - no2);
+}
 
-    printf("\n1.addition\n2.substraction\n3.multiplication\n4.division");
+int mul() // Using function For Multiplication
+{
+            printf("\nmultiplication Is : %d", no1 * no2);
+}
 
-    printf("\nEnter Your Choice : ");
-    scanf("%d", &calc);
+int div() // Using Function For Division
+{
+            printf("\ndivision Is : %f", (float)no1 / (float)no2);
+}
 
+int main() // Main Function
+{
+    printf("\n-----------------Menu----------------\n\n");
+
+    // Choice
+    printf("\n1 addition\n2 substraction\n3 multiplication\n4 .division\n");
+
+    
+    // Pass The Msg To Take User's Input
     printf("\nEnter Your No1 : ");
     scanf("%d", &no1);
 
-    printf("\nEnter Your No2 : ");
+    printf("Enter Your No2 : ");
     scanf("%d", &no2);
 
-    switch (calc)
+    // Pass The Msg To Take Users Choice
+    printf("\nEnter Your Choice : ");
+    scanf("%d", &calc);
+
+    switch (calc) // Using Switch Case
     {
         case 1:
-        printf("\naddition is : %d", no1 + no2);
+        add();
         break;
 
         case 2:
-        printf("\nsubstraction is : %d", no1 - no2);
+        sub();
         break;
         
         case 3:
-        printf("\nmultiplication Is : %d", no1 * no2);
+        mul();
         break;
 
         case 4:
-        printf("\ndivision Is : %f", (float)no1 / (float)no2);
+        div();
         break;
                
         default:
